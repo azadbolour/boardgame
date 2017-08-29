a8cfa87a332c703a11a22c6a1688afffb0f4b8a7
Azad Bolour <azadbolour@bolour.com>
2017-08-28 21:33:42 -0700

    work in progress - begin implementing indexed dictionary for performance

--- haskell-server/src/BoardGame/Util/WordUtil.hs
+++ haskell-server/src/BoardGame/Util/WordUtil.hs
@@ -1,3 +1,9 @@
+--
+-- Copyright 2017 Azad Bolour
+-- Licensed under GNU Affero General Public License v3.0 -
+--   https://github.com/azadbolour/boardgame/blob/master/LICENSE.md
+--
+
 module BoardGame.Util.WordUtil (
     LetterCombo
   , DictWord
