b97867140f33f0d6b1f788bbcdbbf5a78f0063a2
Azad Bolour <azadbolour@bolour.com>
2017-09-12 14:56:34 -0700

    some convenience scripts for docker

--- docker/build-boardgame-docker.sh
+++ docker/build-boardgame-docker.sh
@@ -14,5 +14,12 @@ fi
 
 dockerfile=Dockerfile.boardgame
 
+#
+# Use --no-cache so that the latest source will be pulled.
+# Otherwise docker just compares the pull request with a previously-built and cached layer's 
+# command and since they are the same it will use the old cached layer.
+#
 docker build --no-cache --force-rm=true -f ${dockerfile} -t azadbolour/boardgame:${tag} .
+
+# For convenience in development only - when we change teh docker file after the pull request.
 # docker build --force-rm=true -f ${dockerfile} -t azadbolour/boardgame:${tag} .
