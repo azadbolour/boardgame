7e4a2318ad22a6e174db211eabdbf6889e6004a9
Azad Bolour <azadbolour@bolour.com>
2017-09-11 19:43:33 -0700

    begin cleaning up docker files and build scripts

---
+++ docker/build-boardgame-base-docker.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#
+# Build a base docker image for the game server as of a given git tag.
+# TODO. The dockerfiel should checkout the given tag. Currently it just clones. 
+# OK for our current purposes.
+#
+
+tag=$1
+
+if [ -z "${tag}" ]; then
+  echo "usage: $0 tag"
+  exit 1
+fi
+
+dockerfile=Dockerfile.boardgame-base
+
+docker build --force-rm=true -f ${dockerfile} -t azadbolour/boardgame-base:${tag} 
---
+++ docker/build-boardgame-docker.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#
+# Build a docker image to run the board game.
+# First build the board game from the latest source.
+#
+
+tag=$1
+
+if [ -z "${tag}" ]; then
+  echo "usage: $0 tag"
+  exit 1
+fi
+
+dockerfile=Dockerfile.boardgame
+
+docker build --force-rm=true -f ${dockerfile} -t azadbolour/boardgame:${tag} 
