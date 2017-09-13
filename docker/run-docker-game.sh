f72a9157e2314aff718673bc61a20bf9c5bcc2d7
Azad Bolour <azadbolour@bolour.com>
2017-09-12 18:39:29 -0700

    allow docker container to run in the background

--- docker/run-docker-game.sh
+++ docker/run-docker-game.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-docker run -p 6587:6587 -i -t azadbolour/boardgame:0.2
+# For production deployment.
+# TODO. Try to redirect output or run.sh to a mounted volume in the container.
+# Then can use docker run -d to create a detatched conatiner.
+# And the logs will be on teh host file system.
+nohup docker run -p 6587:6587 --restart on-failure:5 azadbolour/boardgame:0.2 &
+
+# For development.
+# docker run -p 6587:6587 -i -t azadbolour/boardgame:0.2 &
