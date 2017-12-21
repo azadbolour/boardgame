5fa4abd2af2a37ec635794649d4339b410010e38
Azad Bolour <azadbolour@bolour.com>
2017-12-20 17:58:30 -0800

    copy moby dictionary to en-word.sh in deployment

--- scala-server/deploy.sh
+++ scala-server/deploy.sh
@@ -19,4 +19,4 @@ rm -rf scala-server-1.0/
 unzip $outDir/scala-server-1.0.zip
 cd scala-server-1.0
 mkdir dict
-cp ${projectDir}/dict/*.txt dict/
+cp ${projectDir}/dict/moby-english.txt dict/en-words.txt
