e70f9f3d0034677255e4b55e854a31532fa17c52
Azad Bolour <azadbolour@bolour.com>
2018-01-08 19:41:48 -0800

    haskell code cleanup

--- haskell-server/src/BoardGame/Common/Domain/SwissCheeseGrid.hs
+++ haskell-server/src/BoardGame/Common/Domain/SwissCheeseGrid.hs
@@ -63,61 +63,6 @@ data SwissCheeseGrid val = SwissCheeseGrid {
 instance (Show val) => Show (SwissCheeseGrid val)
   where show SwissCheeseGrid {cells} = show cells
 
-mkGrid :: (Height -> Width -> Maybe val) -> Height -> Width -> SwissCheeseGrid val
-mkGrid cellMaker height width =
-  let pointedCellMaker row col = (cellMaker row col, Point row col)
-      grid = Grid.mkGrid pointedCellMaker height width
-  in mkInternal grid
-
-mkEmptyGrid :: Height -> Width -> SwissCheeseGrid val
-mkEmptyGrid = mkGrid (\height width -> Nothing)
-
--- TODO. Any way to add type signatures to functions below for clarity.
--- Compiler did not accept scoped types with type parameters.
-
-mkInternal :: Grid (LocatedValue val) -> SwissCheeseGrid val
-mkInternal grid @ Grid {height, width, cells} =
-  SwissCheeseGrid
-      height
-      width
-      cells
-      get
-      getJusts
-      set
-      setN
-      next
-      prev
-      adjacent
-      pointIsEmpty
-      isolated
-      inBounds
-      farthestNeighbor
-
-    where get point = do
-            (maybeVal, _) <- Grid.get grid point
-            maybeVal
-          set point value =
-            let grid' = Grid.set grid point (value, point)
-            in mkInternal grid'
-          next = Grid.next grid
-          prev = Grid.prev grid
-          adjacent point axis direction =
-            let calcAdj = if direction == 1 then next else prev
-            in calcAdj point axis
-          pointIsEmpty = isNothing . get
-          inBounds = Grid.inBounds grid
-          setN locatedPoints =
-            let addPoint located @ (value, point) = (located, point)
-                locatedPoints' = addPoint <$> locatedPoints
-                grid' = Grid.setN grid locatedPoints'
-            in mkInternal grid'
-          getJusts =
-            let justs = filter (\(may, point) -> isJust may) (Grid.concatGrid grid)
-                mapper (may, point) = (fromJust may, point)
-            in mapper <$> justs
-          isolated point axis = Empty.isEmpty (next point axis) && Empty.isEmpty (prev point axis)
-          farthestNeighbor point axis direction = Grid.get grid point -- TODO. Implement.
-
 instance Empty.Empty (Maybe (LocatedValue val))
   where isEmpty x = isNothing $ join $ fst <$> x
 
@@ -127,4 +72,67 @@ instance Empty.Empty (SwissCheeseGrid val)
 instance Empty.Empty (Maybe val, Point)
   where isEmpty x = let maybe = fst x in isNothing maybe
 
+
+mkGrid :: (Height -> Width -> Maybe val) -> Height -> Width -> SwissCheeseGrid val
+mkGrid cellMaker height width =
+  let pointedCellMaker row col = (cellMaker row col, Point row col)
+      grid = Grid.mkGrid pointedCellMaker height width
+  in mkInternal grid
+
+mkEmptyGrid :: Height -> Width -> SwissCheeseGrid val
+mkEmptyGrid = mkGrid (\height width -> Nothing)
+
+mkInternal :: Grid (LocatedValue val) -> SwissCheeseGrid val
+mkInternal grid @ Grid {height, width, cells} =
+  SwissCheeseGrid
+      height
+      width
+      cells
+      (get' grid)
+      (getJusts' grid)
+      (set' grid)
+      (setN' grid)
+      (Grid.next grid)
+      (Grid.prev grid)
+      (adjacent' grid)
+      (pointIsEmpty' grid)
+      (isolatedInLine' grid)
+      (Grid.inBounds grid)
+      (farthestNeighbor' grid)
+
+type Grid' val = Grid (LocatedValue val)
+
+get' :: Grid' val -> Point -> Maybe val
+get' grid point = do
+  (maybeVal, _) <- Grid.get grid point
+  maybeVal
+
+set' :: Grid' val -> Point -> Maybe val -> SwissCheeseGrid val
+set' grid point value = mkInternal $ Grid.set grid point (value, point)
+
+adjacent' :: Grid' val -> Point -> Axis -> Int -> Maybe (LocatedValue val)
+adjacent' grid point axis direction =
+  (if direction == 1 then Grid.next else Grid.prev) grid point axis
+
+setN' :: Grid' val -> [(Maybe val, Point)] -> SwissCheeseGrid val
+setN' grid locatedValues =
+  mkInternal (Grid.setN grid (addPoint <$> locatedValues))
+    where addPoint (value, point) = ((value, point), point)
+
+getJusts' :: Grid' val -> [(val, Point)]
+getJusts' grid =
+  let justs = filter (isJust . fst) (Grid.concatGrid grid)
+      unjust (just, point) = (fromJust just, point)
+  in unjust <$> justs
+
+pointIsEmpty' :: Grid' val -> Point -> Bool
+pointIsEmpty' grid = isNothing . get' grid
+
+isolatedInLine' :: Grid' val -> Point -> Axis -> Bool
+isolatedInLine' grid point axis =
+  Empty.isEmpty (Grid.next grid point axis)
+    && Empty.isEmpty (Grid.prev grid point axis)
+
+farthestNeighbor' :: Grid' val -> Point -> Axis -> Int -> Maybe (LocatedValue val)
+farthestNeighbor' grid point axis direction = Nothing -- TODO. Implement.
 
