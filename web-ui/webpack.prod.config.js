a7ac69b2af6cc306cdb76188d61c1af9b0c8fca2
Azad Bolour <azadbolour@bolour.com>
2018-01-14 14:52:45 -0800

    remove react hot loader from production bundle - it leads to webpack errors

--- web-ui/webpack.prod.config.js
+++ web-ui/webpack.prod.config.js
@@ -38,7 +38,7 @@ module.exports = {
   module: {
     rules: [{
       test: /\.js$/,
-      use: ['react-hot-loader', 'babel-loader'],
+      use: ['babel-loader'],
       exclude: /node_modules/,
       include: path.join(__dirname, 'src')
     },
