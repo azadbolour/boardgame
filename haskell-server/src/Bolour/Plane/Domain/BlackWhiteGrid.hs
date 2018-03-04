dfa62a4528b9a3fc4bcc33648e5ecdcad25da87a
Azad Bolour <azadbolour@bolour.com>
2018-03-04 10:19:10 -0800

    clean up BlackWhiteGrid.hs

--- haskell-server/src/Bolour/Plane/Domain/BlackWhiteGrid.hs
+++ haskell-server/src/Bolour/Plane/Domain/BlackWhiteGrid.hs
@@ -17,6 +17,13 @@ module Bolour.Plane.Domain.BlackWhiteGrid (
     BlackWhiteGrid(..)
   , mkGrid
   , mkEmptyGrid
+  , height, width, rows, cols, numLines
+  , get, getValues, set, setN
+  , next, prev, adjacent
+  , isBlack, isWhite, isEmpty, hasValue
+  , inBounds
+  , farthestNeighbor, lineNeighbors
+  , segmentsInLine, segmentsAlongAxis, allSegments
   )
   where
 
@@ -38,103 +45,132 @@ import Bolour.Plane.Domain.BlackWhitePoint(BlackWhitePoint, BlackWhitePoint(Blac
 import qualified Bolour.Plane.Domain.BlackWhitePoint as BlackWhitePoint
 
 data BlackWhiteGrid val = BlackWhiteGrid {
-    height :: Height
-  , width :: Width
-  , rows :: [[BlackWhitePoint val]]
-  , cols :: [[BlackWhitePoint val]]
-  -- | Get the cell value of a point - if the point is out of bounds get Black.
-  , get :: Point -> BlackWhite val
-  -- | Get the real values (White and 'Just') of the grid together with their locations.
-  , getValues :: [(val, Point)]
-  -- | Update the value of a point on the grid.
-  , set :: Point -> BlackWhite val -> BlackWhiteGrid val
-  , setN :: [BlackWhitePoint val] -> BlackWhiteGrid val
-  -- | Get the value and location of the next point along an axis, if any.
-  --   Nothing means next is out of bounds.
-  , next :: Point -> Axis -> Maybe (BlackWhitePoint val)
-  , prev :: Point -> Axis -> Maybe (BlackWhitePoint val)
-  , adjacent :: Point -> Axis -> Direction -> Maybe (BlackWhitePoint val)
-
-  , isBlack :: Point -> Bool
-  , isWhite :: Point -> Bool
-  , isEmpty :: Point -> Bool
-  , hasValue :: Point -> Bool
-  , inBounds :: Point -> Bool
-    -- | Get the farthest neighbor of a point along a given axis
-    --   in a given direction.
-    --   Given a point p on a line, a point q on that line
-    --   is considered to be a neighbor of p if there is a contiguous
-    --   non-empty white line segment between p and q (excluding p itself).
-    --   Axis X/Y horizontal/vertical. Direction Axis.forward or Axis.backward.
-    --   The point itself is considered a degenerate neighbour.
-  , farthestNeighbor :: Point -> Axis -> Direction -> Point
-    -- | Get the number of lines of the grid parallel to a given axis:
-    --   X (number of horizontal lines: height), Y (number of vertical lines: width).
-  , numLines :: Axis -> Int
-    -- | Get the range of non-empty white points surrounding a point on a line.
-    --   The point itself is always counted as part of the range whether or
-    --   not it is empty.
-  -- , surroundingRange :: Point -> Axis -> [Point]
-  , lineNeighbors :: Point -> Axis -> Int -> [(val, Point)]
+  innerGrid :: Grid (BlackWhitePoint val)
 }
 
 instance (Show val) => Show (BlackWhiteGrid val)
-  where show BlackWhiteGrid {rows} = show rows
+  where show grid = show $ rows grid
 
 instance Empty.Empty (BlackWhiteGrid val)
-  where isEmpty BlackWhiteGrid {rows} = all Empty.isEmpty (concat rows)
+  where isEmpty grid = all Empty.isEmpty (concat $ rows grid)
 
 instance Empty.Empty (Maybe val, Point)
   where isEmpty x = let maybe = fst x in isNothing maybe
 
-
 mkGrid :: (Height -> Width -> BlackWhite val) -> Height -> Width -> BlackWhiteGrid val
 mkGrid cellMaker height width =
   let pointedCellMaker row col = BlackWhitePoint (cellMaker row col) (Point row col)
-      grid = Grid.mkGrid pointedCellMaker height width
-  in mkInternal grid
+      inner = Grid.mkGrid pointedCellMaker height width
+  in BlackWhiteGrid inner
 
 mkEmptyGrid :: Height -> Width -> BlackWhiteGrid val
 mkEmptyGrid = mkGrid (\height width -> White Nothing)
 
-type Grid' val = Grid (BlackWhitePoint val)
+height :: BlackWhiteGrid val -> Height
+height grid = Grid.height $ innerGrid grid
 
-mkInternal :: Grid' val -> BlackWhiteGrid val
+width :: BlackWhiteGrid val -> Width
+width grid = Grid.width $ innerGrid grid
 
-mkInternal grid =
-  BlackWhiteGrid
-      (Grid.height grid)
-      (Grid.width grid)
-      (Grid.rows grid)
-      (Grid.cols grid)
+rows :: BlackWhiteGrid val -> [[BlackWhitePoint val]]
+rows grid = Grid.rows $ innerGrid grid
 
-      (get' grid)
-      (getValues' grid)
-      (set' grid)
-      (setN' grid)
+cols :: BlackWhiteGrid val -> [[BlackWhitePoint val]]
+cols grid = Grid.cols $ innerGrid grid
 
-      (Grid.next grid)
-      (Grid.prev grid)
-      (Grid.adjacentCell grid)
-
-      (isBlack' grid)
-      (isWhite' grid)
-      (isEmpty' grid)
-      (hasValue' grid)
-      -- (isIsolatedInLine' grid)
-      (Grid.inBounds grid)
-
-      (farthestNeighbor' grid)
-      (Grid.numLines grid)
-      -- (surroundingRange' grid)
-      (lineNeighbors' grid)
-
-get' :: Grid' val -> Point -> BlackWhite val
-get' grid point =
-  let maybeBlackWhitePoint = Grid.get grid point
+-- | Get the cell value of a point - if the point is out of bounds get Black.
+get :: BlackWhiteGrid val -> Point -> BlackWhite val
+get grid point =
+  let maybeBlackWhitePoint = Grid.get (innerGrid grid) point
   in case maybeBlackWhitePoint of
      Nothing -> Black
      Just BlackWhitePoint {value} -> value
+
+-- | Get the real values (White and 'Just') of the grid together with their locations.
+getValues :: BlackWhiteGrid val -> [(val, Point)]
+getValues grid = fromJustWhites (Grid.concatGrid $ innerGrid grid)
+
+-- | Update the value of a point on the grid.
+set :: BlackWhiteGrid val -> Point -> BlackWhite val -> BlackWhiteGrid val
+set grid point value =
+  let inner' = Grid.set (innerGrid grid) point (BlackWhitePoint value point)
+  in BlackWhiteGrid inner'
+
+setN :: BlackWhiteGrid val -> [BlackWhitePoint val] -> BlackWhiteGrid val
+setN grid bwPoints =
+  let inner' = Grid.setN (innerGrid grid) (addPoint <$> bwPoints)
+        where addPoint bwPoint @ BlackWhitePoint {value, point} = (bwPoint, point)
+  in BlackWhiteGrid inner'
+
+-- | Get the value and location of the next point along an axis, if any.
+--   Nothing means next is out of bounds.
+next :: BlackWhiteGrid val -> Point -> Axis -> Maybe (BlackWhitePoint val)
+next grid = Grid.next $ innerGrid grid
+
+prev :: BlackWhiteGrid val -> Point -> Axis -> Maybe (BlackWhitePoint val)
+prev grid = Grid.prev $ innerGrid grid
+
+adjacent :: BlackWhiteGrid val -> Point -> Axis -> Direction -> Maybe (BlackWhitePoint val)
+adjacent grid = Grid.adjacentCell $ innerGrid grid
+
+isBlack :: BlackWhiteGrid val -> Point -> Bool
+isBlack grid point = BlackWhite.isBlack $ get grid point
+
+isWhite :: BlackWhiteGrid val -> Point -> Bool
+isWhite grid point = BlackWhite.isWhite $ get grid point
+
+isEmpty :: BlackWhiteGrid val -> Point -> Bool
+isEmpty grid point = Empty.isEmpty $ get grid point
+
+hasValue :: BlackWhiteGrid val -> Point -> Bool
+hasValue grid point = BlackWhite.hasValue $ get grid point
+
+inBounds :: BlackWhiteGrid val -> Point -> Bool
+inBounds grid = Grid.inBounds $ innerGrid grid
+
+-- | Get the farthest neighbor of a point along a given axis
+--   in a given direction.
+--
+--   Given a point p on a line, a point q on that line
+--   is considered to be a neighbor of p if there is a contiguous
+--   non-empty white line segment between p and q (excluding p itself).
+--   Axis X/Y horizontal/vertical. Direction Axis.forward or Axis.backward.
+--   The point itself is considered a degenerate neighbour.
+--   TODO. The point should not be a neighbor. Return Maybe.
+farthestNeighbor :: BlackWhiteGrid val -> Point -> Axis -> Direction -> Point
+farthestNeighbor grid point axis direction =
+   if not (adjHasValue grid point axis direction) then point
+   else fromJust $ find neighborIsValueBoundary neighbors
+      where
+        neighborIsValueBoundary neighbor = valueBoundary grid neighbor axis direction
+        dimension = numLines grid axis
+        neighbors = Point.nthNeighbor point axis direction <$> [1 .. dimension - 1]
+
+-- | Get the number of lines of the grid parallel to a given axis:
+--   X (number of horizontal lines: height), Y (number of vertical lines: width).
+numLines :: BlackWhiteGrid val -> Axis -> Int
+numLines grid = Grid.numLines $ innerGrid grid
+
+-- | Get all the colinear neighbors in a given direction along a given axis
+--   ordered in increasing value of the line index (excluding the point
+--   itself). A colinear point is considered a neighbor if it has a real value,
+--   and is adjacent to the given point, or recursively adjacent to a neighbor.
+lineNeighbors :: BlackWhiteGrid val -> Point -> Axis -> Int -> [(val, Point)]
+lineNeighbors grid point axis direction =
+  let farthestPoint = farthestNeighbor grid point axis direction
+      Point {row = pointRow, col = pointCol} = point
+      Point {row = farRow, col = farCol} = farthestPoint
+      range pos limit =
+        -- Exclude the point itself.
+        if direction == Axis.forward then [pos + 1 .. limit] else [limit .. pos - 1]
+      neighbors =
+        case axis of
+          Axis.X -> Point pointRow <$> colRange
+            where colRange = range pointCol farCol
+          Axis.Y -> flip Point pointCol <$> rowRange
+            where rowRange = range pointRow farRow
+      blackWhitePoints = catMaybes $ Grid.get (innerGrid grid) <$> neighbors
+  in fromJustWhites blackWhitePoints
 
 fromJustWhites :: [BlackWhitePoint val] -> [(val, Point)]
 fromJustWhites bwPoints =
@@ -145,99 +181,36 @@ fromJustWhites bwPoints =
                   Just v -> Just (v, point)
   in catMaybes $ maybePair <$> bwPoints
 
-getValues' :: Grid' val -> [(val, Point)]
-getValues' grid = fromJustWhites (Grid.concatGrid grid)
+adjValue :: BlackWhiteGrid val -> Point -> Axis -> Int -> Maybe val
+adjValue grid point axis direction =
+  do -- Maybe
+    BlackWhitePoint {value} <- adjacent grid point axis direction
+    BlackWhite.fromWhite value
 
-set' :: Grid' val -> Point -> BlackWhite val -> BlackWhiteGrid val
-set' grid point value = mkInternal $ Grid.set grid point (BlackWhitePoint value point)
+adjHasValue :: BlackWhiteGrid val -> Point -> Axis -> Int -> Bool
+adjHasValue grid point axis direction = isJust $ adjValue grid point axis direction
 
-setN' :: Grid' val -> [BlackWhitePoint val] -> BlackWhiteGrid val
-setN' grid bwPoints =
-  mkInternal (Grid.setN grid (addPoint <$> bwPoints))
-    where addPoint bwPoint @ BlackWhitePoint {value, point} = (bwPoint, point)
-
--- | Out of bounds is considered to be black!
-isBlack' :: Grid' val -> Point -> Bool
-isBlack' grid point =
-  let bw = get' grid point
-  in case bw of
-     Black -> True
-     _ -> False
-
-isWhite' :: Grid' val -> Point -> Bool
-isWhite' grid point = not (isBlack' grid point)
-
-isEmpty' :: Grid' val -> Point -> Bool
-isEmpty' grid point =
-  let bw = get' grid point
-  in case bw of
-     Black -> False
-     White maybe -> isNothing maybe
-
-hasValue' :: Grid' val -> Point -> Bool
-hasValue' grid point =
-  let bw = get' grid point
-  in BlackWhite.isJustWhite bw
-
-adjHasValue :: Grid' val -> Point -> Axis -> Int -> Bool
-adjHasValue grid point axis direction =
-  let maybe = do
-                BlackWhitePoint {value} <- Grid.adjacentCell grid point axis direction
-                BlackWhite.fromWhite value
-  in isJust maybe
-
-valuedNotValuedTransitionPoint :: Grid' val -> Point -> Axis -> Int -> Bool
-valuedNotValuedTransitionPoint grid point axis direction =
+valueBoundary :: BlackWhiteGrid val -> Point -> Axis -> Int -> Bool
+valueBoundary grid point axis direction =
   let adjacentHasValue = adjHasValue grid point axis direction
-  in hasValue' grid point && not adjacentHasValue
+  in hasValue grid point && not (adjHasValue grid point axis direction)
 
--- | Point itself is considered a degenerate neighbor.
-farthestNeighbor' :: Grid' val -> Point -> Axis -> Int -> Point
-farthestNeighbor' grid point axis direction =
-   if not (adjHasValue grid point axis direction) then point
-   else fromJust $ find valuedToNotValued neighbors
-      where
-        valuedToNotValued pnt = valuedNotValuedTransitionPoint grid pnt axis direction
-        dimension = Grid.numLines grid axis
-        neighbors = Point.nthNeighbor point axis direction <$> [1 .. dimension - 1]
-
--- | Get all the colinear neighbors in a given direction along a given axis
---   ordered in increasing value of the line index (excluding the point
---   itself). A colinear point is considered a neighbor if it has a real value,
---   and is adjacent to the given point, or recursively adjacent to a neighbor.
-lineNeighbors' :: Grid' val -> Point -> Axis -> Int -> [(val, Point)]
-lineNeighbors' grid point axis direction =
-  let farthestPoint = farthestNeighbor' grid point axis direction
-      Point {row = pointRow, col = pointCol} = point
-      Point {row = farRow, col = farCol} = farthestPoint
-      range pos limit =
-        -- Exclude the point itself.
-        if direction == Axis.forward then [pos + 1 .. limit] else [limit .. pos - 1]
-      points =
-        case axis of
-          Axis.X -> Point pointRow <$> colRange
-            where colRange = range pointCol farCol
-          Axis.Y -> flip Point pointCol <$> rowRange
-            where rowRange = range pointRow farRow
-      blackWhitePoints = catMaybes $ Grid.get grid <$> points
-  in fromJustWhites blackWhitePoints
-
-linesAlongAxis :: Grid' val -> Axis -> [[BlackWhite val]]
+linesAlongAxis :: BlackWhiteGrid val -> Axis -> [[BlackWhite val]]
 linesAlongAxis grid axis =
-  let Grid {rows = blackWhiteRows} = BlackWhitePoint.value <$> grid
+  let Grid {rows = blackWhiteRows} = BlackWhitePoint.value <$> innerGrid grid
   in case axis of
        Axis.X -> blackWhiteRows
        Axis.Y -> transpose blackWhiteRows
 
-segmentsAlongAxis :: Grid' val -> Axis -> [LineSegment val]
+segmentsAlongAxis :: BlackWhiteGrid val -> Axis -> [LineSegment val]
 segmentsAlongAxis grid axis = do
-  lineNumber <- [0 .. Grid.numLines grid axis - 1]
+  lineNumber <- [0 .. numLines grid axis - 1]
   segmentsInLine grid axis lineNumber
 
-allSegments :: Grid' val -> [LineSegment val]
+allSegments :: BlackWhiteGrid val -> [LineSegment val]
 allSegments grid = segmentsAlongAxis grid Axis.X ++ segmentsAlongAxis grid Axis.Y
 
-segmentsInLine :: Grid' val -> Axis -> Int -> [LineSegment val]
+segmentsInLine :: BlackWhiteGrid val -> Axis -> Int -> [LineSegment val]
 segmentsInLine grid axis lineNumber =
   let line = linesAlongAxis grid axis !! lineNumber
       len = length line
