5a754d2b6c9dfbcc7f8c268169d67813ed24c7c9
Azad Bolour <azadbolour@bolour.com>
2018-03-19 21:03:14 -0700

    begin integrating detect-it for device detection

--- web-ui/src/util/BrowserUtil.js
+++ web-ui/src/util/BrowserUtil.js
@@ -4,6 +4,8 @@
  *   https://github.com/azadbolour/boardgame/blob/master/LICENSE.md
  */
 
+import detectIt from 'detect-it';
+
 export const hasDragAndDrop = function() {
   let div = window.document.createElement('div');
   return ('draggable' in div) || ('ondragstart' in div && 'ondrop' in div);
@@ -13,3 +15,43 @@ export const isTouchDevice = ('ontouchstart' in window) || ((Navigator.maxTouchP
 
 export const isMobile = /Mobi/.test(navigator.userAgent);
 
+/**
+ * Get the name of the pointing device to use.
+ * @param preferredDevice The user's preferred device ('mouse' or 'touch') -
+ *    returned if available in current browser.
+ * @returns 'mouse' or 'touch' or undefined.
+ */
+export const pointingDevice = function(preferredDevice) {
+  if (isDeviceAvailable(preferredDevice))
+    return preferredDevice;
+  return detectPrimaryDevice()
+};
+
+function isDeviceAvailable(deviceName) {
+  if (deviceName === undefined)
+    return false;
+
+  switch (deviceName) {
+    case 'mouse':
+      return (detectIt.hasMouse = true) || (detectIt.primaryInput === 'mouse');
+    case 'touch':
+      return (detectIt.hasTouch = true) || (detectIt.primaryInput === 'touch');
+    default:
+      return false;
+  }
+}
+
+/**
+ * If no primary input is detectable then default to mouse if available,
+ * otherwise to touch if available.
+ */
+function detectPrimaryDevice() {
+  let primary = detectIt.primaryInput;
+  if (primary === 'mouse' || primary === 'touch')
+    return primary;
+  if (detectIt.hasMouse)
+    return 'mouse';
+  if (detectIt.hasTouch)
+    return 'touch';
+  return undefined;
+}
