b4db7485cc5b0033decd1cc0b624b4e6ae1f62e6
Azad Bolour <azadbolour@bolour.com>
2019-11-12 20:49:08 -0800

    fixed no dragging diplayed - regression from earlier react dnd version

--- boardgame-ui/src/component/PieceComponent.js
+++ boardgame-ui/src/component/PieceComponent.js
@@ -8,13 +8,21 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useDrag } from 'react-dnd';
 // import logger from "../util/Logger";
 // import {stringify} from "../util/Logger";
 // import * as Piece from "../domain/Piece";
 import {isDead} from "../domain/Piece";
+
+import {
+  DragSource,
+  ConnectDragSource,
+  ConnectDragPreview,
+  DragSourceConnector,
+  DragSourceMonitor,
+  DragPreviewImage,
+} from 'react-dnd';
+
 const ItemTypes = require('./DragDropTypes').ItemTypes;
-// const DragSource = require('react-dnd').DragSource;
 
 const letterStyle = {
   fontSize: 15,
@@ -24,34 +32,34 @@ const letterStyle = {
   userSelect: 'none'
 };
 
-// const pieceDragger = {
-//   /**
-//    * Return the drag item: an arbitrary plain JS object
-//    * providing information about the dragged element.
-//    * The drag item is distinct from the source component
-//    * of the drag.
-//    */
-//   beginDrag: function (props) {
-//     let piece = props.piece;
-//     // console.log(`begin drag: ${stringify(piece)}`);
-//     // Must return a plain object.
-//     let item = {
-//       value: piece.value,
-//       id: piece.id,
-//     };
-//     return item;
-//   },
-//
-//   canDrag(props, monitor) {
-//     let can = props.canMovePiece(props.piece);
-//     // console.log(`canDrag: ${can} - ${stringify(props.piece)}`);
-//     return can;
-//   },
-//
-//   endDrag: function(props, monitor, component) {
-//      // console.log(`end drag - dropped: ${monitor.didDrop()}`);
-//   }
-// };
+const pieceDragger = {
+  /**
+   * Return the drag item: an arbitrary plain JS object
+   * providing information about the dragged element.
+   * The drag item is distinct from the source component
+   * of the drag.
+   */
+  beginDrag: function (props) {
+    let piece = props.piece;
+    // console.log(`begin drag: ${stringify(piece)}`);
+    // Must return a plain object.
+    let item = {
+      value: piece.value,
+      id: piece.id,
+    };
+    return item;
+  },
+
+  canDrag(props, monitor) {
+    let can = props.canMovePiece(props.piece);
+    // console.log(`canDrag: ${can} - ${stringify(props.piece)}`);
+    return can;
+  },
+
+  endDrag: function(props, monitor, component) {
+     // console.log(`end drag - dropped: ${monitor.didDrop()}`);
+  }
+};
 
 /**
  * Get dnd properties to be injected into the drag source component.
@@ -62,96 +70,96 @@ const letterStyle = {
  * @returns Plain object including properties to inject into
  * the dragged source component.
  */
-// function injectedDragSourceProperties(connect, monitor) {
-//   return {
-//     connectDragSource: connect.dragSource(),
-//     connectDragPreview: connect.dragPreview(),
-//     isDragging: monitor.isDragging()
-//   }
-// }
+function injectedDragSourceProperties(connect, monitor) {
+  return {
+    connectDragSource: connect.dragSource(),
+    connectDragPreview: connect.dragPreview(),
+    isDragging: monitor.isDragging()
+  }
+}
 
 /**
  * The piece. A draggable item.
  */
+// class PieceComponent extends React.Component {
 const PieceComponent = (props) => {
 
-  const piece = props.piece;
-  const draggedItem = {
-    type: ItemTypes.PIECE,
-    value: piece.value,
-    id: piece.id,
-  };
+  // static propTypes = {
+  //
+  //   /**
+  //    * The immutable state of the piece.
+  //    * TODO. How to add isRequired with type constraint?
+  //    */
+  //   piece: PropTypes.object.isRequired,
+  //
+  //   /**
+  //    * Function that takes a piece and determines if it can be moved
+  //    * (based on its current location and move status).
+  //    */
+  //   canMovePiece: PropTypes.func.isRequired,
+  //
+  //   /**
+  //    * Injected - obtained from the 'collect' function.
+  //    */
+  //   connectDragSource: PropTypes.func.isRequired,
+  //
+  //   connectDragPreview: PropTypes.func.isRequired,
+  //
+  //   /**
+  //    * Injected - obtained from the 'collect' function.
+  //    */
+  //   isDragging: PropTypes.bool.isRequired
+  // };
 
-  const [{isDragging}, drag] = useDrag({
-    item: draggedItem,
-    collect: monitor => ({
-      isDragging: !!monitor.isDragging(),
-    }),
-  });
 
-  // let connectDragSource = props.connectDragSource;
-  // let connectDragPreview = props.connectDragPreview;
-  // let isDragging = props.isDragging;
-  let letter = props.piece.value;
-  if (isDead(letter))
-    letter = ""; // Do not render "dead" piece.
-  // let worth = Piece.worths[letter];
-
-  let theDiv =
-    <div
-      ref={drag}
-      style={{
-        opacity: isDragging ? 0.5 : 1,
-        fontSize: 15,
-        fontWeight: 'bold',
-        cursor: 'move',
-        zIndex: 10
-      }}
-    >
-      <div style={letterStyle}>
-        {letter}
+    let connectDragSource = props.connectDragSource;
+    let connectDragPreview = props.connectDragPreview;
+    let isDragging = props.isDragging;
+    let letter = props.piece.value;
+    if (isDead(letter))
+      letter = ""; // Do not render "dead" piece.
+    // let worth = Piece.worths[letter];
+  
+    // let theDiv =
+    return (
+      <>
+      <div
+        ref={isDragging ? connectDragPreview : connectDragSource}
+        style={{
+          opacity: isDragging ? 0.5 : 1,
+          fontSize: 15,
+          fontWeight: 'bold',
+          cursor: 'move',
+          zIndex: 10
+        }}
+      >
+        <div style={letterStyle}>
+          {letter}
+        </div>
       </div>
-    </div>;
+      </>
+    );
 
-  return theDiv;
+    // return connectDragSource(theDiv);
+    // if (!isDragging)
+    //   return connectDragSource(theDiv);
+    // else
+    //   // return connectDragPreview(connectDragSource(theDiv));
+    //   return connectDragPreview(theDiv);
 
-  // if (!isDragging)
-  //   return connectDragSource(theDiv);
-  // else
-  //   return connectDragPreview(connectDragSource(theDiv));
-  //   // return connectDragPreview(theDiv);
 };
 
+export default DragSource(
+  ItemTypes.PIECE, pieceDragger,
+  (connect, monitor) => ({
+    connectDragSource: connect.dragSource(),
+    connectDragPreview: connect.dragPreview(),
+    isDragging: monitor.isDragging(),
+  })
+)(PieceComponent)
 
-PieceComponent.propTypes = {
 
-  /**
-   * The immutable state of the piece.
-   * TODO. How to add isRequired with type constraint?
-   */
-  piece: PropTypes.object.isRequired,
 
-  /**
-   * Function that takes a piece and determines if it can be moved
-   * (based on its current location and move status).
-   */
-  canMovePiece: PropTypes.func.isRequired,
-
-  /**
-   * Injected - obtained from the 'collect' function.
-   */
-  // connectDragSource: PropTypes.func.isRequired,
-
-  // connectDragPreview: PropTypes.func.isRequired,
-
-  /**
-   * Injected - obtained from the 'collect' function.
-   */
-  // isDragging: PropTypes.bool.isRequired
-};
-
-export default PieceComponent;
-
-// export default DragSource(ItemTypes.PIECE, pieceDragger, injectedDragSourceProperties)(PieceComponent);
+// DragSource(ItemTypes.PIECE, pieceDragger, injectedDragSourceProperties)(PieceComponent);
 
 // export {PieceComponent as OriginalPieceComponent};
